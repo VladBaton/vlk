137c80b6e6971d4e93748dffa933b66b59646510
VladBaton <vladstalker2000@yandex.ru>
2025-05-10 16:20:47 +0300

    CHSU-0007: заполнение таблиц product и productDescription

---
+++ src/main/resources/db/sql/CHSU-0007-handbook.sql
@@ -0,0 +1,98 @@
+insert into product (productId, "productSysName")
+    values (1000, 'Личное страхование');
+
+insert into productDescription (productDescriptionId, productId, fieldSysName, fieldName, fieldDescription)
+    values (1001, 1000, 'longField01', 'isTickProtection', 'Включена ли страховка на случай укуса клеща');
+
+insert into productDescription (productDescriptionId, productId, fieldSysName, fieldName, fieldDescription)
+    values (1002, 1000, 'longField02', 'isIllnessProtection', 'Включена ли страховка на болезни');
+
+insert into productDescription (productDescriptionId, productId, fieldSysName, fieldName, fieldDescription)
+    values (1003, 1000, 'longField03', 'isInjuryProtection', 'Включена ли страховка на случай травм');
+
+insert into product (productId, "productSysName")
+    values (2000, 'Защита дома');
+
+insert into productDescription (productDescriptionId, productId, fieldSysName, fieldName, fieldDescription)
+    values (2001, 2000, 'doubleField01', 'houseArea', 'Площадь дома');
+
+insert into productDescription (productDescriptionId, productId, fieldSysName, fieldName, fieldDescription)
+    values (2002, 2000, 'longField01', 'numberOfFloors', 'Этажность дома');
+
+insert into productDescription (productDescriptionId, productId, fieldSysName, fieldName, fieldDescription)
+    values (2003, 2000, 'stringField01', 'fiasCode', 'Адрес дома по ФИАС');
+
+insert into productDescription (productDescriptionId, productId, fieldSysName, fieldName, fieldDescription)
+    values (2004, 2000, 'stringField02', 'district', 'Название субъекта РФ');
+
+insert into productDescription (productDescriptionId, productId, fieldSysName, fieldName, fieldDescription)
+    values (2005, 2000, 'stringField03', 'settlement', 'Населённый пункт');
+
+insert into productDescription (productDescriptionId, productId, fieldSysName, fieldName, fieldDescription)
+    values (2006, 2000, 'stringField04', 'street', 'Название улицы');
+
+insert into productDescription (productDescriptionId, productId, fieldSysName, fieldName, fieldDescription)
+    values (2007, 2000, 'stringField05', 'houseNumber', 'Номер дома');
+
+insert into product (productId, "productSysName")
+    values (3000, 'Защита квартиры');
+
+insert into productDescription (productDescriptionId, productId, fieldSysName, fieldName, fieldDescription)
+    values (3001, 3000, 'doubleField01', 'flatArea', 'Площадь квартиры');
+
+insert into productDescription (productDescriptionId, productId, fieldSysName, fieldName, fieldDescription)
+    values (3002, 3000, 'longField01', 'numberOfFloors', 'Этажность дома');
+
+insert into productDescription (productDescriptionId, productId, fieldSysName, fieldName, fieldDescription)
+    values (3003, 3000, 'stringField01', 'fiasCode', 'Адрес дома по ФИАС');
+
+insert into productDescription (productDescriptionId, productId, fieldSysName, fieldName, fieldDescription)
+    values (3004, 3000, 'stringField02', 'district', 'Название субъекта РФ');
+
+insert into productDescription (productDescriptionId, productId, fieldSysName, fieldName, fieldDescription)
+    values (3005, 3000, 'stringField03', 'settlement', 'Населённый пункт');
+
+insert into productDescription (productDescriptionId, productId, fieldSysName, fieldName, fieldDescription)
+    values (3006, 3000, 'stringField04', 'street', 'Название улицы');
+
+insert into productDescription (productDescriptionId, productId, fieldSysName, fieldName, fieldDescription)
+    values (3007, 3000, 'stringField05', 'houseNumber', 'Номер дома');
+
+insert into productDescription (productDescriptionId, productId, fieldSysName, fieldName, fieldDescription)
+    values (3008, 3000, 'longField02', 'flatNumber', 'Номер квартиры');
+
+insert into product (productId, "productSysName")
+    values (4000, 'Каско');
+
+insert into productDescription (productDescriptionId, productId, fieldSysName, fieldName, fieldDescription)
+    values (4001, 4000, 'stringField01', 'carBrand', 'Марка авто');
+
+insert into productDescription (productDescriptionId, productId, fieldSysName, fieldName, fieldDescription)
+    values (4002, 4000, 'stringField02', 'carModel', 'Модель авто');
+
+insert into productDescription (productDescriptionId, productId, fieldSysName, fieldName, fieldDescription)
+    values (4003, 4000, 'stringField03', 'carNumber', 'Гос номер авто');
+
+insert into productDescription (productDescriptionId, productId, fieldSysName, fieldName, fieldDescription)
+    values (4004, 4000, 'stringField04', 'VIN', 'VIN-номер авто');
+
+insert into productDescription (productDescriptionId, productId, fieldSysName, fieldName, fieldDescription)
+    values (4005, 4000, 'longField01', 'enginePower', 'Мощность двигателя');
+
+insert into product (productId, "productSysName")
+    values (5000, 'Осаго');
+
+insert into productDescription (productDescriptionId, productId, fieldSysName, fieldName, fieldDescription)
+    values (5001, 5000, 'stringField01', 'carBrand', 'Марка авто');
+
+insert into productDescription (productDescriptionId, productId, fieldSysName, fieldName, fieldDescription)
+    values (5002, 5000, 'stringField02', 'carModel', 'Модель авто');
+
+insert into productDescription (productDescriptionId, productId, fieldSysName, fieldName, fieldDescription)
+    values (5003, 5000, 'stringField03', 'carNumber', 'Гос номер авто');
+
+insert into productDescription (productDescriptionId, productId, fieldSysName, fieldName, fieldDescription)
+    values (5004, 5000, 'stringField04', 'VIN', 'VIN-номер авто');
+
+insert into productDescription (productDescriptionId, productId, fieldSysName, fieldName, fieldDescription)
+    values (5005, 5000, 'longField01', 'enginePower', 'Мощность двигателя');
